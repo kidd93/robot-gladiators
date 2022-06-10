61b88300740b3b7f657932b3ac6eb3344fa0468d
kelsie kidd <kelsiekidd93@gmail.com>
2022-06-10 11:26:33 -0500

    robot-gladiators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,19 +1,34 @@
-// this creates a function named "fight"
-var playername = window.prompt("what is your robot's name?");
+var playerName = window.prompt("what is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playername);
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// this will do math and log 20
-console.log(10+10);
-
-// what i this?
-
-console.log("Our robots name is " + kelsie);
-
-function fight() {
-    window.alert("The fight has begun!");
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
 }
 
-// fight();
+enemyHealth = enemyHealth - playerAttack;
+console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
+
+if (enemyHealth <= 0){
+    window.alert(enemyName + " has died!");
+}
+else {
+    window.alert(enemyName + " still has " + enemyHealth + " health left.");
+}
+
+player = playerHealth - enemyAttack;
+console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+
+if (playerHealth <= 0) {
+    window.alert(playerName + " has died!");
+}
+else {
+    window.alert(playerName + " still has " + playerHealth + " health left.");
+} 

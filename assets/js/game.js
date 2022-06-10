4fc612c35b610f2d29f628230a7353b3732cb8cc
kelsie kidd <kelsiekidd93@gmail.com>
2022-06-10 15:38:34 -0500

    fight or skip prompt

--- assets/js/game.js
+++ assets/js/game.js
@@ -11,24 +11,35 @@ var enemyAttack = 12;
 var fight = function() {
     window.alert("Welcome to Robot Gladiators!");
 }
+var promptFight = window.prompt("Would you like to Fight or Skip this battle? Enter 'Fight' or 'Skip' to choose.");
 
+if (promptFight === "fight" || promptFight === "FIGHT") {
+   
 enemyHealth = enemyHealth - playerAttack;
-console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-);
 
-if (enemyHealth <= 0){
-    window.alert(enemyName + " has died!");
-}
-else {
-    window.alert(enemyName + " still has " + enemyHealth + " health left.");
-}
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining.");
 
-player = playerHealth - enemyAttack;
-console.log(enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    } else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+playerHealth = playerHealth - enemyAttack;
+
+console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining.");
 
 if (playerHealth <= 0) {
     window.alert(playerName + " has died!");
+} else {
+    window.alert(playerName + " still has " + playerHealth + "health left.");
 }
-else {
-    window.alert(playerName + " still has " + playerHealth + " health left.");
-} 
+}
+
+else if (promptFight === "skip" || promptFight === "SKIP") {
+    window.alert(playerName + " has chosen to skip the fight!");
+} else {
+    window.alert("You need to choose a valid option. Try again!");
+}

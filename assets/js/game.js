0290a3d2dd20c0ea094a5aecda2f0fa95bbec3b2
kelsie kidd <kelsiekidd93@gmail.com>
2022-06-10 09:06:10 -0500

    robot-gladiators

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! Javascript is running!")
+// this creates a function named "fight"
+var playername = window.prompt("what is your robot's name?");
+
+console.log(playername);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+// this will do math and log 20
+console.log(10+10);
+
+// what i this?
+
+console.log("Our robots name is " + kelsie);
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
